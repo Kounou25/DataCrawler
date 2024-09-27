1f5fc171af1d246b4ce4adefb8436f84db81b70b
Kounou Gnimassou Gilbert <kounougilbert@MacBook-Air-de-Kounou.local>
2024-09-27 11:40:34 +0100

    j'ai ajouter le code pour afficher les information et ca fonctionne tres bien,camment.........😀

---
+++ lib/widgets/Home_Page.dart
@@ -0,0 +1,35 @@
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+  final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        title: Center(
+          child: Text(
+            "Urban Crawler",
+            style: TextStyle(),
+          ),
+        ),
+      ),
+      body: FutureBuilder<AndroidDeviceInfo>(
+          future: deviceInfo.androidInfo,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done &&
+                snapshot.hasData) {
+              AndroidDeviceInfo InfoTel = snapshot.data!;
+              return Center(
+                child: Text(
+                    "Marque: ${InfoTel.brand}\n Model: ${InfoTel.model}🤣\n id const: ${InfoTel.id}\n interName: ${InfoTel.device}\n materiel : ${InfoTel.hardware}\n Fabricant: ${InfoTel.manufacturer}\n"),
+              );
+            }
+            return CircularProgressIndicator();
+          }),
+    );
+  }
+}
